99802653e92532abb6ae13b6857624775c0101ba
JenniferFulton <jennifer.reeves8@gmail.com>
2022-10-02 18:59:34 -0400

    scorecard form created

--- hand/scorecard/urls.py
+++ hand/scorecard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page),
+    path('new_game', views.new_game),
+    path('scorecard', views.scorecard),
 ]
--- hand/scorecard/views.py
+++ hand/scorecard/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+def new_game(request):
+    return render(request, 'new_game.html')
+
+def scorecard(request):
+    return render(request, 'scorecard.html')
